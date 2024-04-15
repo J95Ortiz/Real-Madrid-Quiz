f0ae037c89931e06df889ee57f2c7a5a3df60e14
Joe Ortiz <Se_sema_95@hotmail.com>
2024-04-15 13:53:34 +0000

    Added questions structure to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,510 @@
 console.log("Hello from the console");
 
 //Creates an array with all the questions and answers
-//50 questions stored, but only 10 displayed per game 
+//50 questions stored, but only 10 displayed per game
 //The answers array in each question will hold the options and whether they're right or wrong
-let questions = []
+let questions = [
+  {
+    question:
+      "Which Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "2 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "3 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "4 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "5 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "6 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "7 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "8 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "9 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "10 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "11 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "12 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "13 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "14 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "15 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "16 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "17 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "18 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "19 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "20 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "21 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "22 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "23 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "24 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "25 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "26 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "27 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "28 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "29 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "30 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "31 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "32 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "33 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "34 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "35 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "36 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "37 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "38 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "39 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "40 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "41 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "42 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "43 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "44 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "45 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "46 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "47 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "48 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "49 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+  {
+    question:
+      "50 Player was the only spanish player signed by Florentino in his first period as president?",
+    answers: [
+      { text: "Sergio Ramos", correct: true },
+      { text: "Iker Casillas", correct: false },
+      { text: "Raul", correct: false },
+      { text: "Isco", correct: false },
+    ],
+  },
+];
 
 //Variable quizQuestion will display the question text in #question h2
 let quizQuestion = document.getElementById("question");
@@ -17,4 +518,3 @@ let nextBtn = document.getElementById("next-btn");
 
 //Sets the score as 0 when the user starts the quiz and increases if question is answered correctly
 let score = 0;
-
